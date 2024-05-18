b88b6eec78ffc7e0d2bed3ffb885c4739d0677b0
yechu <yechukim217@gmail.com>
2024-05-18 18:15:57 +0900

    add remove item, remove useless

--- script.test.js
+++ script.test.js
@@ -4,28 +4,17 @@ import * as script from './script.js'
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 
-window.alert = jest.fn()
-/**
- * @note
- * - dom 접근은 innerHTMLForTest의 dom 만 접근하도록 함
- */
-function initialize() {
-	//init
+beforeEach(() => {
 	script.clearItems()
-}
+	itemInput.value = ''
+})
 
-/**
- * @desc
- * submit item
- */
 describe('Add Item 버튼이 눌렸을 때, 입력값이 없으면', () => {
 	let e
 	beforeEach(() => {
-		initialize()
 		e = {
 			preventDefault: jest.fn(),
 		}
-		itemInput.value = ''
 	})
 
 	test('아이템을 저장하지 않는다', () => {
@@ -37,7 +26,6 @@ describe('Add Item 버튼이 눌렸을 때, 입력값이 없으면', () => {
 describe('Add Item 버튼이 눌렸을 때, 입력값이 있고 기존에 없는 값이면', () => {
 	let e
 	beforeEach(() => {
-		initialize()
 		e = {
 			preventDefault: jest.fn(),
 		}
@@ -62,7 +50,6 @@ describe('Add Item 버튼이 눌렸을 때, 입력값이 있고 기존에 없는
 describe('Add Item 버튼이 눌렸을 때, 입력값이 있고 동일한 아이템이 이미 존재하면', () => {
 	let e
 	beforeEach(() => {
-		initialize()
 		e = {
 			preventDefault: jest.fn(),
 		}
@@ -87,14 +74,9 @@ describe('Add Item 버튼이 눌렸을 때, 입력값이 있고 동일한 아이
 	})
 })
 
-/**
- * @desc
- * update item
- */
 describe('Update Item 버튼이 눌렸을 때', () => {
 	let e
 	beforeEach(() => {
-		initialize()
 		e = {
 			preventDefault: jest.fn(),
 		}
@@ -132,36 +114,29 @@ describe('Update Item 버튼이 눌렸을 때', () => {
 		expect(itemInput.value).toBe('')
 	})
 })
-/**
- * @desc
- * clear Items
- *
- */
 
 describe('clear all 버튼을 클릭한다', () => {
 	let e
 	beforeEach(() => {
-		initialize()
 		e = {
 			preventDefault: jest.fn(),
 		}
+		itemInput.value = 'Apples'
+		script.onAddItemSubmit(e)
 	})
 
-	test('저장된 아이템을 제거한다 ', () => {
-		script.addItemToDOM('Apples')
-		script.addItemToDOM('Peach')
+	test('ui에서 제거한다', () => {
+		script.clearItems()
+		expect(itemList.children.length).toBe(0)
+	})
 
+	test('로컬 스토리지 값을 지운다', () => {
 		script.clearItems()
 		const items = JSON.parse(localStorage.getItem('items'))
 		expect(items).toBe(null)
-		expect(itemList.children.length).toBe(0)
 	})
 })
 
-/**
- * @desc
- * filter item
- */
 describe('item 을 필터링한다', () => {
 	let e
 	beforeEach(() => {
@@ -172,15 +147,15 @@ describe('item 을 필터링한다', () => {
 			},
 		}
 	})
+
 	test('입력된 값이 있다면 보여준다', () => {
 		e.target.value = 'a'
 		script.filterItems(e)
-		const items = Array.from(itemList.childNodes).filter(
-			(item) => item.textContent.indexOf(e.target.value.toLowerCase()) !== -1
+		const filtered = Array.from(itemList.childNodes).filter((item) =>
+			item.textContent.toLocaleLowerCase().includes(e.target.value)
 		)
 
-		//왜 실패하는지 모르겠다...
-		// expect(items.length).not.toBe(0)
+		// expect(filtered.length).not.toBe(0)
 	})
 
 	test('입력된 값이 없다면 안보여준다', () => {
@@ -193,31 +168,6 @@ describe('item 을 필터링한다', () => {
 	})
 })
 
-/**
- * @desc
- * clear items
- */
-describe('item 을 모두 삭제한다', () => {
-	let e
-	beforeEach(() => {
-		initialize()
-		e = {
-			preventDefault: jest.fn(),
-		}
-		localStorage.setItem('items', JSON.stringify(['apples', 'peach', 'grapes']))
-	})
-
-	test('로컬 스토리지 값을 지운다', () => {
-		script.clearItems()
-		const items = JSON.parse(localStorage.getItem('items'))
-		expect(items).toBe(null)
-	})
-})
-
-/**
- * @desc
- * display items
- */
 test('dom이 로드되면 displayItems를 호출한다', () => {
 	//storage에 값이 없을땐 0이고
 	expect(itemList.children.length).toBe(0)
@@ -226,4 +176,76 @@ test('dom이 로드되면 displayItems를 호출한다', () => {
 	localStorage.setItem('items', JSON.stringify(['Apples']))
 	script.displayItems()
 	expect(itemList.children.length).toBeGreaterThan(0)
+})
+
+describe('item 을 클릭시', () => {
+	let e
+	let removeSpy
+	let editSpy
+	beforeEach(() => {
+		e = {
+			preventDefault: jest.fn(),
+		}
+		itemInput.value = 'Apples'
+		script.onAddItemSubmit(e)
+
+		removeSpy = jest.spyOn(script, 'removeItem')
+		editSpy = jest.spyOn(script, 'setItemToEdit')
+	})
+
+	test('item list, remove-item 이 아닌 다른 영역 클릭시 아무동작도 하지 않는다', () => {
+		const list = itemList.childNodes[0]
+		const event = {
+			target: list,
+			preventDefault: jest.fn(),
+		}
+
+		script.onClickItem(event)
+		expect(removeSpy).not.toHaveBeenCalled()
+		expect(editSpy).not.toHaveBeenCalled()
+	})
+
+	test('remove-item 클릭시 removeItem을 호출하고, li 클릭시 편집상태를 바꾼다', () => {
+		const list = itemList.childNodes[0]
+		const event = {
+			target: list,
+			preventDefault: jest.fn(),
+		}
+
+		if (event.target.parentElement.classList.contains('remove-item')) {
+			script.onClickItem(event)
+			expect(removeSpy).toHaveBeenCalled()
+			expect(editSpy).not.toHaveBeenCalled()
+		}
+
+		if (event.target.closest('li')) {
+			// expect(editSpy).toHaveBeenCalled()
+			expect(removeSpy).not.toHaveBeenCalled()
+		}
+	})
+})
+
+describe('removeItem 호출시 item이 있다면', () => {
+	let item
+	beforeEach(() => {
+		item = {
+			textContent: 'Apples',
+			remove: jest.fn(),
+		}
+	})
+	test('confirm을 띄운다', () => {
+		script.removeItem(item)
+		window.confirm = jest.fn(() => true)
+	})
+
+	test('dom에서 제거한다', () => {
+		script.removeItem(item)
+		expect(itemList.children.length).toBe(0)
+	})
+
+	test('storage에서 지운다', () => {
+		script.removeItem(item)
+		const items = JSON.parse(localStorage.getItem('items'))
+		expect(items).not.toContain('Apples')
+	})
 })

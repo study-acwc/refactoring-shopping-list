74ea8787ace31eb19572767f0bbd205aa50f8270
sweetptios <sweetptios@gmail.com>
2024-05-21 17:23:59 +0900

    style: 3항 연사자로 조건문을 더 간결하게 표현

--- script.js
+++ script.js
@@ -171,11 +171,7 @@ export function filterItems(e) {
 
   items.forEach((item) => {
     const itemName = item.firstChild.textContent.toLowerCase();
-    if (itemName.indexOf(text) != -1) {
-      item.style.display = 'flex';
-    } else {
-      item.style.display = 'none';
-    }
+    item.style.display = itemName.indexOf(text) != -1 ? 'flex' : 'none';
   });
 }
 

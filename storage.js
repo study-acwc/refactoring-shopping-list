14e69a4251444644db7a20d3df334ad901faa857
sweetptios <sweetptios@gmail.com>
2024-06-01 17:25:34 +0900

    refactor: localStorage 레코드 캡슐화

--- storage.js
+++ storage.js
@@ -1,2 +1,9 @@
+export class Storage {
+    constructor() {
+        this._storage = localStorage;
+    }
+}
+
+const aStorage = new Storage();
 
 export function rawDateOfStorage() { return aStorage._storage; }
